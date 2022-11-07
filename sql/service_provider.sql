64efb110d6e13817806bc46de8a54ddb092821c5
Charvi-Sharma <csharma71c@gmail.com>
2022-11-07 14:22:22 +0530

    initial commit

--- sql/service_provider.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.service_provider
-(
-    sp_id integer NOT NULL DEFAULT nextval('service_provider_sp_id_seq'::regclass),
-    sp_type text COLLATE pg_catalog."default",
-    sp_speciality text COLLATE pg_catalog."default",
-    sp_headline text COLLATE pg_catalog."default",
-    sp_description text COLLATE pg_catalog."default",
-    CONSTRAINT service_provider_pkey PRIMARY KEY (sp_id)
-)
